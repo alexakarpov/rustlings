847f1f42fe3486392b854c6eb850401aeb812ed9
Alexandre Karpov <alexakarpov@gmail.com>
2022-07-01 02:00:49 -0400

    guessing

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("you've guessed: {guess}");
+
+    let x = 2;
+    let y = 3;
+    println!("x is {x}, y is {y}")
 }
