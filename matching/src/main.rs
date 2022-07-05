9ec8f223db3ff457ef7e07ccfc9e25b0ceb948b9
Alex Karpov <alexakarpov@gmail.com>
2022-07-05 00:03:20 -0400

    options

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -6,12 +6,13 @@ enum Coin {
     Quarter,
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
+fn value_in_cents(coin: Option<Coin>) -> u8 {
     match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Some(Coin::Penny) => 1,
+        Some(Coin::Nickel) => 5,
+        Some(Coin::Dime) => 10,
+        Some(Coin::Quarter) => 25,
+        None => 0
     }
 }
 
@@ -20,11 +21,11 @@ fn main() {
     let n = Coin::Nickel;
     let q1 = Coin::Quarter;
     let q2 = Coin::Quarter;
-    println!("{}", value_in_cents(p));
-    println!("{}", value_in_cents(n));
-    println!("{}", value_in_cents(q1));
+    println!("{}", value_in_cents(Some(p)));
+    println!("{}", value_in_cents(Some(n)));
+    println!("{}", value_in_cents(Some(q1)));
     // the code below won't work after the variables are moved
     // in value_in_cents calls, unless Copy and Clone are derived
-    assert_ne!(p,n);
-    assert_eq!(value_in_cents(q1), value_in_cents(q2));
+    assert_ne!(Some(p), Some(n));
+    assert_eq!(value_in_cents(Some(q1)), value_in_cents(Some(q2)));
 }
