735ba7ab1b47f25d39b0fdd146bdaa8cc913a4fe
Alexandre Karpov <alexakarpov@gmail.com>
2022-07-04 22:24:32 -0400

    started enum work

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,11 @@
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+    // route(IpAddr::V4);
+    // route(IpAddr::V6);
 }
