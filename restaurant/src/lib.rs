615aa9c1d84b35094e7dd235e0f377adfcb2409a
Alex Karpov <alexakarpov@gmail.com>
2022-07-07 23:03:22 -0400

    backend in lib and pub eat in front

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -6,6 +8,8 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+pub const GREETING: &'static str = "Hallo, GREETIN is pub consts from lib.rs";
 
 mod front_of_house {
     mod hosting {
@@ -21,4 +25,50 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    mod back_of_house {
+        pub enum Appetizer {
+            Soup,
+            Salad,
+        }
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    dbg!("eat_at_restaurant");
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = dbg!(back_of_house::Breakfast::summer("Rye"));
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
